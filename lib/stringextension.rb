6d6870d701740e1e0171b58781fb783cb36a4c5a
Hermann <herm@Hermann-Fas-MacBook-Pro.local>
2014-06-04 23:25:13 +0200

    Added String#upcase_unicode(), a method that allows to change characters including some non-ASCII characters (as used in many European languages) from lowercase to uppercase spelling.
    Not tested! Not efficient yet!

--- lib/stringextension.rb
+++ lib/stringextension.rb
@@ -102,5 +102,57 @@ class String
     self.to_ascii.gsub(/\s+/, '_').gsub(/[^\w\-_\.]/, '_')
   end
 
+  # Return the uppercase pendent of a letter.
+  # This includes non-ASCII characters!
+  #--
+  # Highly inefficient. 
+  # Consider moving the Hash uppercase_letter to a class variable
+  # and thus assign it only once.
+  #++
+  def upcase_unicode()
+    uppercase_letter = {
+        "\u00DF" => 'SS',      #  ß
+        "\u00E0" => "\u00C0",  #  à   c3 a0   LATIN SMALL LETTER A WITH GRAVE
+        "\u00E1" => "\u00C1",  #  á   c3 a1   LATIN SMALL LETTER A WITH ACUTE
+        "\u00E2" => "\u00C2",  #  â   c3 a2   LATIN SMALL LETTER A WITH CIRCUMFLEX
+        "\u00E3" => "\u00C3",  #  ã   c3 a3   LATIN SMALL LETTER A WITH TILDE
+        "\u00E4" => "\u00C4",  #  ä   c3 a4   LATIN SMALL LETTER A WITH DIAERESIS
+        "\u00E5" => "\u00C5",  #  å   c3 a5   LATIN SMALL LETTER A WITH RING ABOVE
+        "\u00E6" => "\u00C6",  #  æ   c3 a6   LATIN SMALL LETTER AE
+        "\u00E7" => "\u00C7",  #  ç   c3 a7   LATIN SMALL LETTER C WITH CEDILLA
+        "\u00E8" => "\u00C8",  #  è   c3 a8   LATIN SMALL LETTER E WITH GRAVE
+        "\u00E9" => "\u00C9",  #  é   c3 a9   LATIN SMALL LETTER E WITH ACUTE
+        "\u00EA" => "\u00CA",  #  ê   c3 aa   LATIN SMALL LETTER E WITH CIRCUMFLEX
+        "\u00EB" => "\u00CB",  #  ë   c3 ab   LATIN SMALL LETTER E WITH DIAERESIS
+        "\u00EC" => "\u00CC",  #  ì   c3 ac   LATIN SMALL LETTER I WITH GRAVE
+        "\u00ED" => "\u00CD",  #  í   c3 ad   LATIN SMALL LETTER I WITH ACUTE
+        "\u00EE" => "\u00CE",  #  î   c3 ae   LATIN SMALL LETTER I WITH CIRCUMFLEX
+        "\u00EF" => "\u00CF",  #  ï   c3 af   LATIN SMALL LETTER I WITH DIAERESIS
+        "\u00F0" => "\u00D0",  #  ð   c3 b0   LATIN SMALL LETTER ETH
+        "\u00F1" => "\u00D1",  #  ñ   c3 b1   LATIN SMALL LETTER N WITH TILDE
+        "\u00F2" => "\u00D2",  #  ò   c3 b2   LATIN SMALL LETTER O WITH GRAVE
+        "\u00F3" => "\u00D3",  #  ó   c3 b3   LATIN SMALL LETTER O WITH ACUTE
+        "\u00F4" => "\u00D4",  #  ô   c3 b4   LATIN SMALL LETTER O WITH CIRCUMFLEX
+        "\u00F5" => "\u00D5",  #  õ   c3 b5   LATIN SMALL LETTER O WITH TILDE
+        "\u00F6" => "\u00D6",  #  ö   c3 b6   LATIN SMALL LETTER O WITH DIAERESIS
+        "\u00F8" => "\u00D8",  #  ø   c3 b8   LATIN SMALL LETTER O WITH STROKE
+        "\u00F9" => "\u00D9",  #  ù   c3 b9   LATIN SMALL LETTER U WITH GRAVE
+        "\u00FA" => "\u00DA",  #  ú   c3 ba   LATIN SMALL LETTER U WITH ACUTE
+        "\u00FB" => "\u00DB",  #  û   c3 bb   LATIN SMALL LETTER U WITH CIRCUMFLEX
+        "\u00FC" => "\u00DC",  #  ü   c3 bc   LATIN SMALL LETTER U WITH DIAERESIS
+        "\u00FD" => "\u00DE",  #  ý   c3 bd   LATIN SMALL LETTER Y WITH ACUTE
+        "\u00FE" => "\u00DF",  #  þ   c3 be   LATIN SMALL LETTER THORN
+    }
+    new_str = ''
+    self.each_char do |c|
+      new_str << (uppercase_letter[c]) ? uppercase_letter[c] : c.upcase()
+    end
+    new_str
+  end
+
+  def upcase_unicode!()
+    self.replace( self.upcase_unicode() )
+  end
+
 end
 
--- lib/stringextension/version.rb
+++ lib/stringextension/version.rb
@@ -1,3 +1,3 @@
 module Stringextension
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
