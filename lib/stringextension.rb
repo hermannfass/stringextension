c6b088072bd9c715d211f6bfef159f32ed5e1fc3
Hermann <herm@Hermann-Fas-MacBook-Pro.local>
2014-06-09 14:23:42 +0200

    Added to_bin, to_hex, to_dec methods. The methods to_bin, to_hex, to_dec, and all other previous methods that utilized the UnicodeString class are now added by meta programming the String class.

--- lib/stringextension.rb
+++ lib/stringextension.rb
@@ -7,139 +7,95 @@
 # and thereby make the string 7-bit or URL safe.
 class String
 
-  # Word wrap to a specified number of maximum characters per line.
-  # In case the test already contains newlines those will result
-  # in an additional empty line each to keep paragraphs visible.
-  def wrap( cols = 72 )
-    self.gsub!(/(.{1,#{cols}})( +|$\n?)|(.{1,#{cols}})/, "\\1\\3\n")
-    #           -----------   -------   -----------       ------
-    #           |             |         |                 |
-    #           0-72 chars followed by: |                 |
-    #                         |         |                 |
-    #                         spaces or a newline or (if that does not exist)
-    #                                   |                 |
-    #                                  0-72 chars not followed by whitespace
-    #                                                     |
-    #       Either 0-72 chars (followed by space or not)--/
-  end
-
-  # Indent a text block, i.e. prepend a number of spaces (1st argument)
-  # to the beginning of each line. If the text is already indented, i.e.
-  # if lines are already prepended by whitespace, this can be removed
-  # (default) or not (set 2nd argument to true for this).
-  def indent( cols = 2, leave_existing_indentation = false )
-    old_str = self
-    new_str = ''
-    self.each_line do |l|
-      l.lstrip! unless (leave_existing_indentation)
-      new_str << ' '*cols + l
-    end
-    self.replace( new_str )
-  end
-  
-  # Replace all letters of this String that are not 7-bit safe by an
-  # adequate String value from the ASCII character set.
-  # This concerns mainly accented letters, umlauts, or ligatures
-  # including the German 'Sharp S'.
-  def to_ascii
-    str = String.new( self )
-    replacements = {
-      ['á', 'à', 'â', 'ã'] => 'a',
-      ['ä', 'æ'] => 'ae',
-      ['Á', 'À', 'Â', 'Ã'] => 'A',
-      ['Ä', 'Æ'] => 'Ae',
-      ['ç'] => 'c',
-      ['Ç'] => 'C',
-      ['é', 'è', 'ê', 'ë'] => 'e',
-      ['É', 'È', 'Ê', 'Ë'] => 'E',
-      ['í', 'ì', 'î', 'ï'] => 'i',
-      ['Í', 'Ì', 'Î', 'Ï'] => 'I',
-      ['ó', 'ò', 'ô', 'õ'] => 'o',
-      ['ö', 'œ'] => 'oe',
-      ['Ó', 'Ò', 'Ô', 'Õ'] => 'O',
-      ['Ö', 'Œ'] => 'Oe',
-      ['ú', 'ù', 'û'] => 'u',
-      ['ü'] => 'ue',
-      ['Ú', 'Ù', 'Û',] => 'U',
-      ['Ü'] => 'Ue',
-      ['ß'] => 'ss',
-      ['ñ'] => 'n',
-      ['Ñ'] => 'N',
-      ['¼'] => 'Quarter',
-      ['½'] => 'Half'
-    }
-    replacements.each do |c, ascii|
-      c.each do |s|
-        str.gsub!(s, ascii)
-      end
-    end
-    return str
-  end
-
-  # Like String#to_ascii(), but makes the String instance itself
-  # 7-bit safe. Use this only if you want to change this
-  # instance of String (vs. receiving a 7-bit safe copy of it).
-  def to_ascii!
-    self.replace( self.to_ascii() )
-  end
-  
-    # Return a representation of this String's content with all those characters
-    # removed that could cause problems or inefficiencies when being used in
-    # URLs.
-    # This is also useful when constructing filenames. (Despite of the fact
-    # that most OSs support non-7bit characters and spaces it still recommended
-    # to make filenames URL safe from the very beginning.)
-    # Spaces are replaced by hyphens (not underscores) to support SEO.
-    def urlify
-        self.to_ascii.gsub(/\s+/, '-').gsub(/[^\w\-_]/, '_')
+    # Add some methods of the UnicodeString class to the String class.
+    # This implicitely instanciates UnicodeString in the background.
+    [
+        :to_bin, :to_hex, :to_dec,
+        :to_ascii, :to_ascii!,
+        :to_base_filename, :to_base_filename!,
+        :urlify, :urlify!,
+        :unicode_upcase, :unicode_upcase!,
+        :unicode_downcase, :unicode_downcase!,
+        :unicode_capitalize, :unicode_capitalize!
+    ].each do |method|
+        send :define_method, method do
+            UnicodeString.new(self).send method
+        end
     end
 
-    # Return a representation of this String's content that is useful to
-    # construct a file or directory name. Similar to
-    # urlify, but the dot is removed (leaving it for the suffix only)
-    # and words are connected by underscore characters (not hyphens).
-    def to_base_filename
-        self.to_ascii.gsub(/\s+/, '_').gsub(/[^\w\-_\.]/, '_')
+    # Word wrap to a specified number of maximum characters per line.
+    # In case the test already contains newlines those will result
+    # in an additional empty line each to keep paragraphs visible.
+    def wrap( cols = 72 )
+        self.gsub!(/(.{1,#{cols}})( +|$\n?)|(.{1,#{cols}})/, "\\1\\3\n")
+        #           -----------   -------   -----------       ------
+        #           |             |         |                 |
+        #           0-72 chars followed by: |                 |
+        #                         |         |                 |
+        #                         spaces or a newline or (if that does not exist)
+        #                                   |                 |
+        #                                  0-72 chars not followed by whitespace
+        #                                                     |
+        #       Either 0-72 chars (followed by space or not)--/
     end
 
-
-    # Return the all-uppercase version of this String.
-    # This treats also non-ASCII characters well!
-    def unicode_upcase
-        UnicodeString.new(self).unicode_upcase
+    # Indent a text block, i.e. prepend a number of spaces (1st argument)
+    # to the beginning of each line. If the text is already indented, i.e.
+    # if lines are already prepended by whitespace, this can be removed
+    # (default) or not (set 2nd argument to true for this).
+    def indent( cols = 2, leave_existing_indentation = false )
+        old_str = self
+        new_str = ''
+        self.each_line do |l|
+            l.lstrip! unless (leave_existing_indentation)
+            new_str << ' '*cols + l
+        end
+        self.replace( new_str )
     end
 
-    # Change the value of this String to its all-uppercase version.
-    # This treats also non-ASCII characters well!
-    def unicode_upcase!
-        self.replace( self.unicode_upcase )
+end # End class String
+
+
+
+# Class to analyse single characters. All methods work also on
+# multibyte characters and thereby allow the analysis of Unicode
+# characters.
+class UnicodeChar < String
+
+    # Returns a string represention of the binary value of this UnicodeChar
+    # instance.
+    # The parameter byte_separator contains a string that is used to separate
+    # the individual bytes in case this UnicodeChar describes a multibyte
+    # character.
+    # This separator can be overwritten by a string parameter passed to this
+    # method.
+    def to_bin( byte_separator = ' ' )
+        self.unpack("B8B8B8B8").reject {|b| b == ''}.join(byte_separator)
     end
 
-    # Return the all-lowercase version of this String.
-    # This treats also non-ASCII characters well!
-    def unicode_downcase
-        UnicodeString.new(self).unicode_downcase
+    # Returns a string representing the hexadecimal value of this Unicode
+    # character.
+    # The parameter byte_separator contains a string that is used to separate
+    # the individual bytes in case this UnicodeChar describes a multibyte
+    # character.
+    def to_hex( byte_separator = ' ' )
+        self.unpack("H2H2H2H2").reject {|h| h == ''}.join(byte_separator).upcase
     end
 
-    # Change the value of this String to its all-lowercase version.
-    # This treats also non-ASCII characters well!
-    def unicode_downcase!
-        self.replace( self.unicode_downcase )
+    # Returns a string representing the decimal value of this Unicode character.
+    # For multibyte characters the representation of the individual bytes are
+    # separated by a single space character.
+    # This separator can be overwritten by a string parameter.
+    def to_dec( byte_separator = ' ' )
+        self.unpack("C*").join(byte_separator)
     end
 
-    # Returns the value of this UnicodeString with the first character
-    # - if it is a letter - turned into uppercase letter and the prepending
-    # letters turned into the corresponding lowercase letter.
-    # This treats also non-ASCII initials well.
-    def unicode_capitalize
-        UnicodeString.new(self).unicode_capitalize
+    # Test if this Unicode character consists of multiple bytes.
+    def multibyte?
+        self.bytesize > self.size
     end
 
-    def unicode_capitalize!
-        self.replace( self.unicode_capitalize )
-    end
-
-end
+end  # End class UnicodeChar
 
 
 # Class to represent Strings that contain characters beyond ASCII.
@@ -188,7 +144,130 @@ class UnicodeString < String
     # ('SS') in uppercase.
     @@uppercase_letters.each_key do |c|
         @@lowercase_letters[@@uppercase_letters[c]] = c
-        @@lowercase_letters['ß'] = 'ß'
+    end
+    @@lowercase_letters['ß'] = 'ß'
+
+    @@ascii_replacements = {
+        ['á', 'à', 'â', 'ã'] => 'a',
+        ['ä', 'æ'] => 'ae',
+        ['Á', 'À', 'Â', 'Ã'] => 'A',
+        ['Ä', 'Æ'] => 'Ae',
+        ['ç'] => 'c',
+        ['Ç'] => 'C',
+        ['é', 'è', 'ê', 'ë'] => 'e',
+        ['É', 'È', 'Ê', 'Ë'] => 'E',
+        ['í', 'ì', 'î', 'ï'] => 'i',
+        ['Í', 'Ì', 'Î', 'Ï'] => 'I',
+        ['ó', 'ò', 'ô', 'õ'] => 'o',
+        ['ö', 'œ'] => 'oe',
+        ['Ó', 'Ò', 'Ô', 'Õ'] => 'O',
+        ['Ö', 'Œ'] => 'Oe',
+        ['ú', 'ù', 'û'] => 'u',
+        ['ü'] => 'ue',
+        ['Ú', 'Ù', 'Û',] => 'U',
+        ['Ü'] => 'Ue',
+        ['ß'] => 'ss',
+        ['ñ'] => 'n',
+        ['Ñ'] => 'N',
+        ['¼'] => 'Quarter',
+        ['½'] => 'Half'
+    }
+
+    # Returns the content of this instance, replacing all characters that are
+    # not 7-bit-safe by an adequate String value from the ASCII character set.
+    # This concerns mainly accented letters, umlauts, or ligatures
+    # including the German 'Sharp S'.
+    def to_ascii()
+        str = String.new( self )
+        @@ascii_replacements.each do |c, ascii|
+            c.each do |s|
+                str.gsub!(s, ascii)
+            end
+        end
+        return str
+    end
+
+    # Sets the value of this instance to the return value of its
+    # to_ascii() method.
+    def to_ascii!()
+        self.replace( self.to_ascii )
+    end
+
+    # Return a representation of this instance's content with all characters
+    # that could cause problems or inefficiencies as part of a URL.
+    # Spaces are replaced by hyphens (not underscores) to support SEO.
+    def urlify
+        self.to_ascii.gsub(/\s+/, '-').gsub(/[^\w\-_]/, '_')
+    end
+
+    # Replaces the value of this instance with the return value of
+    # urlify().
+    def urlify!
+        self.replace( self.urlify )
+    end
+
+    # Return a representation of this instance's content with all characters
+    # removed that could cause problems or inefficiencies as part of a
+    # filename. Similar to
+    # urlify, but the dot is removed (leaving it for the suffix only)
+    # and words are connected by underscore characters (not hyphens).
+    def to_base_filename()
+        self.to_ascii.gsub(/\s+/, '_').gsub(/[^\w\-_\.]/, '_')
+    end
+
+    # Replaces the value of this instance by the return value of
+    # to_base_filename().
+    def to_base_filename!()
+        self.replace( self.to_base_filename() )
+    end
+
+    # Returns all characters from this UnicodeString instance
+    # as an Array of UnicodeChar objects.
+    # A multibyte character will be one element of this Array
+    # (we are in a $KCODE=='u' environment).
+    def unicode_chars
+      result = []
+      self.split(//).each do |char|
+          result.push( UnicodeChar.new(char) )
+      end
+      result
+    end
+
+
+    # Returns a binary representation (sequence of 0 and 1 digits)
+    # of this UnicodeString instance, i.e. of this text.
+    # The parameter char_separator is used to separate the binary
+    # representations of individual characters.
+    # The parameter byte_separator is used to separate the binary
+    # representation of the individual bytes of a multibyte string
+    # where applicable.
+    def to_bin( char_separator = ' ', byte_separator = '.' )
+        bin_chars = self.unicode_chars.collect { |uc| uc.to_bin(byte_separator) }
+        bin_chars.join(char_separator)
+    end
+
+    # Returns a hexadecimal representation of this UnicodeString
+    # instance, i.e. of this text.
+    # The parameter char_separator is used to separate the
+    # hexadecimal representations of individual characters.
+    # The parameter byte_separator is used to separate the
+    # hexadecimal representation of the individual bytes of
+    # a multibyte string where applicable.
+    def to_hex( char_separator = ' ', byte_separator = '.' )
+        hex_chars = self.unicode_chars.collect { |uc| uc.to_hex(byte_separator) }
+        hex_chars.join(char_separator)
+    end
+
+    # Returns a decimal representation of this UnicodeString
+    # instance, i.e. of this text.
+    # The parameter char_separator is used to separate the
+    # hexadecimal representations of individual characters.
+    # The parameter byte_separator is used to separate the
+    # hexadecimal representation of the individual bytes of
+    # a multibyte string where applicable.
+    def to_dec( char_separator = ' ', byte_separator = '.' )
+        dec_chars = self.unicode_chars.collect { |uc| uc.to_dec(byte_separator) }
+        dec_chars.join(char_separator)
     end
 
     # Return the all-uppercase pendent of this UnicodeString.
@@ -201,6 +280,12 @@ class UnicodeString < String
         new_str
     end
 
+    # Replace the value of this UnicodeString by the return value
+    # of its unicode_upcase().
+    def unicode_upcase!()
+        self.replace( self.unicode_upcase )
+    end
+
     # Return the all-lowercase version of this UnicodeString.
     # This treats also non-ASCII characters well!
     def unicode_downcase() 
@@ -211,7 +296,13 @@ class UnicodeString < String
         new_str
     end
 
-    # Returns the value of this UnicodeString with the first character
+    # Replace the value of this instance by the return value of
+    # its unicode_downcase() method.
+    def unicode_downcase!()
+        self.replace( self.unicode_downcase )
+    end
+
+    # Returns the value of this instance with the first character
     # turned into uppercase letter (if it is a letter) and the prepending
     # letters turned into the corresponding lowercase letter.
     # This treats also non-ASCII initials well.
@@ -219,5 +310,11 @@ class UnicodeString < String
         self[0].unicode_upcase() + self[1..self.length].unicode_downcase()
     end
 
-end
+    # Replace the value of this instance by the return value of
+    # its unicode_capitalize() method.
+    def unicode_capitalize!()
+        self.replace( self.unicode_capitalize() )
+    end
+
+end # End class UnicodeString
 
